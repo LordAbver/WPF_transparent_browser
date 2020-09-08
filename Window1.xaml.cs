622a60765cfafca70978161bc518334d27c40f21
LordAbver <37892965+LordAbver@users.noreply.github.com>
2020-09-08 17:08:34 +0300

    Initial release

---
+++ Window1.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace WebBrowserOnTransparentWindow
+{
+    /// <summary>
+    /// Window visually hosting a web browser control; using transparency
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1(): this(OverlayStyle.WinForms)
+        {
+        }
+        public Window1(OverlayStyle style)
+        {
+            InitializeComponent();
+
+            if (style == OverlayStyle.WPF)
+            {
+                WebBrowserOverlay wbo = new WebBrowserOverlay(_webBrowserPlacementTarget);
+                WebBrowser wb = wbo.WebBrowser;
+                wb.Navigate(new Uri("http://yandex.ru"));
+
+                _showOtherButton.Content = "Show WinForms-based overlay";
+            }
+            else if (style == OverlayStyle.WinForms)
+            {
+                WebBrowserOverlayWF wbo = new WebBrowserOverlayWF(_webBrowserPlacementTarget);
+                System.Windows.Forms.WebBrowser wb = wbo.WebBrowser;
+                wb.Navigate(new Uri("http://yandex.ru"));
+
+                _showOtherButton.Content = "Show WPF-based overlay";
+            }
+
+            _showOtherButton.Click += delegate
+            {
+                new Window1(style == OverlayStyle.WinForms ? OverlayStyle.WPF : OverlayStyle.WinForms).Show();                
+            };
+        }
+
+        public enum OverlayStyle { 
+            WPF, // Can't use opacity.
+            WinForms // Flicker on resize when opacity is used without DWM composition
+        };
+
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            base.OnMouseMove(e);
+
+            if (e.LeftButton == MouseButtonState.Pressed &&
+                // For some reason Slider doesn't seem to mark MouseMove events as handled.
+                VisualTreeHelper.HitTest(_opacitySlider, e.GetPosition(_opacitySlider)) == null)
+            {
+                DragMove();
+            }
+        }
+    }
+}
